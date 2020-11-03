17c72056e536e601b10e0368ca9baf32c19aa386
John Ray <john@trychec.com>
2020-11-03 15:45:13 -0500

    create a protected Post Read endpoint

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,7 +4,24 @@ const Post = require("@/models/Post");
 const middleware = require('@/middleware');
 const service = require("@/services/posts");
 
+// callback trigger for postId param
+router.param(":postId", function(req, res, next, postId) {
+  return Post.findById(postId)
+    .exec()
+    .then(function(post) {
+      if (!post) {
+        return res.sendStatus(404);
+      }
+      req.post = post;
+      return next();
+    })
+    .catch(next);
+});
+
 // Create a post, uploading a video/image file to Cloudinary
 router.post('/', middleware.sessionRequireUser, ...service.create);
+
+// Read a post, only giving authorization to the
+router.get('/:postId', middleware.sessionRequireUser, ...service.read);
 
 module.exports = router;
--- services/posts.js
+++ services/posts.js
@@ -14,12 +14,27 @@ const multerCloudinaryMiddleware = require("@/middleware/multerCloudinary");
 
 module.exports = {
   /**
+   * TODO: add ability for followers to have access to the read
+   * post endpoint for a user they follow.
+   */
+  read: [
+    (req, res, next) => {
+      // only give authorization to user of post
+      if (req.user.id !== req.post.id) {
+        return next({
+          name: "ForbiddenError",
+        })
+      }
+      return res.json({ success: true, post: req.post.jsonSerialize() })
+    },
+  ],
+  /**
    * create a new image post
    */
   create: [
     multerCloudinaryMiddleware.single('postFile'),
     function(req, res, next) {
-      debugger;
+
       const post = new Post({
         user: req.user.id,
         title: req.body.title,
@@ -33,7 +48,7 @@ module.exports = {
       });
 
       return post.save().then(post => {
-        res.json({ success: true, ...post.jsonSerialize(), })
+        res.json({ success: true, post: post.jsonSerialize(), })
       })
       .catch(next)
     },

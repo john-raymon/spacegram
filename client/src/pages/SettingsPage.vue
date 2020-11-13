f6f08a56c98f53c4261acd64577f4b3159e43b46
John Ray <john@trychec.com>
2020-11-12 21:55:12 -0500

    add link to settings password nested route

--- client/src/pages/SettingsPage.vue
+++ client/src/pages/SettingsPage.vue
@@ -5,7 +5,7 @@
     </h1>
     <ul class="w-full space-y-4 text-base tracking-wider text-white">
       <li tabIndex="0">
-        <div class="settings-item">
+        <router-link to="/settings/password" class="settings-item">
           <div class="flex items-center">
             <div class="w-5 h-5 mr-4">
               <KeyIconSvg />
@@ -17,7 +17,7 @@
           <div class="w-6 h-6">
             <RightArrowIcon />
           </div>
-        </div>
+        </router-link>
       </li>
       <li tabIndex="0">
         <div class="settings-item">

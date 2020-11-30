aee812fdf9391f0287822ae78dde59cf3df9fcfe
John Ray <john@trychec.com>
2020-11-29 19:52:49 -0500

    create modal component

---
+++ client/src/components/Modal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    @click.self="$emit('close-modal')"
+    class="modal-component flex items-center justify-center bg-black h-full w-full top-0 left-0 bottom-0 z-50 opacity-60 fixed bg-opacity-50 px-4"
+  >
+    <div class="rounded-md w-full max-w-md bg-white h-auto shadow-md p-6">
+      <div class="header-container flex flex-row justify-between">
+        <slot name="heading">
+          <h1 class="text-black">
+            Title
+          </h1>
+        </slot>
+        <button @click="$emit('close-modal')" class="w-6 h-6 text-black fill-current">
+          <CloseIconSvg />
+        </button>
+      </div>
+      <slot name="body">
+        <div class="w-full h-40 text-black">
+          Nothing here
+        </div>
+      </slot>
+    </div>
+  </div>
+</template>
+<script>
+import CloseIconSvg from "@/assets/svgs/close-icon-svg.svg";
+
+export default {
+  name: "Modal",
+  components: {
+    CloseIconSvg
+  }
+};
+</script>
